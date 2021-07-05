492ac16f6f090bf857a07ed25c789fc7489cbe55
Marko Krizan <markokrizan64@gmail.com>
2021-07-05 15:45:20 +0200

    Center align featured post image

--- frontend/components/cover-image.js
+++ frontend/components/cover-image.js
@@ -1,17 +1,17 @@
-import cn from 'classnames'
-import Link from 'next/link'
+import cn from "classnames";
+import Link from "next/link";
 
 export default function CoverImage({ title, coverImage, slug }) {
   const image = (
     <img
       src={coverImage?.sourceUrl}
-      className={cn('shadow-small', {
-        'hover:shadow-medium transition-shadow duration-200': slug,
+      className={cn("shadow-small", {
+        "hover:shadow-medium transition-shadow duration-200": slug,
       })}
     />
-  )
+  );
   return (
-    <div className="sm:mx-0">
+    <div className="sm:mx-0 flex justify-center">
       {slug ? (
         <Link as={`/posts/${slug}`} href="/posts/[slug]">
           <a aria-label={title}>{image}</a>
@@ -20,5 +20,5 @@ export default function CoverImage({ title, coverImage, slug }) {
         image
       )}
     </div>
-  )
+  );
 }
