db616148f7cb7e5aabdf7e48c9b748239157ca35
Marko Krizan <markokrizan64@gmail.com>
2021-07-08 11:43:06 +0200

    Reduce title sizes

--- frontend/components/intro.js
+++ frontend/components/intro.js
@@ -1,7 +1,7 @@
 export default function Intro() {
   return (
     <section className="flex-col md:flex-column flex md:justify-between mt-16 mb-16 md:mb-12">
-      <h1 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
+      <h1 className="text-6xl md:text-7xl font-bold tracking-tighter leading-tight md:pr-8">
         devedu.rs
       </h1>
       <h3>Junior dev blog</h3>
